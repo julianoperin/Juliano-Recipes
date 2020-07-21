03314b5dc819c5bdeb99bec8ade666b3dba2d466
‘Juliano <julianoperins@gmail.com>
2020-07-21 03:19:58 -0400

    fetched data from the api

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,9 @@ async function getResults(query) {
 
   const KEY = "7e1a99ec4243020ada15269bce85b86b";
 
-  const URL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${KEY}`;
+  const URL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${KEY}&from=0&to=30`;
+
+  //   "https://api.edamam.com/search?q=chicken&app_id=13231e53&app_key=7e1a99ec4243020ada15269bce85b86b&from=0&to=30"
 
   try {
     const res = await axios(URL);
